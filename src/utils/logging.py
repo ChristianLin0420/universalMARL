61e58ff4082950334f669f331c101723e4e81f5d
ChristianLin0420 <crlc112358@gmail.com>
2022-06-19 21:54:55 +0800

    finished vanilla transformer testing

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -45,7 +45,7 @@ class Logger:
                 continue
             i += 1
             window = 5 if k != "epsilon" else 1
-            item = "{:.4f}".format(np.mean([x[1] for x in self.stats[k][-window:]]))
+            item = "{:.4f}".format(np.mean([x[1].cpu() for x in self.stats[k][-window:]]))
             log_str += "{:<25}{:>8}".format(k + ":", item)
             log_str += "\n" if i % 4 == 0 else "\t"
         self.console_logger.info(log_str)
