0bb7156c253f7dd6ffd4f863872c2cbfda17afc8
ChristianLin0420 <crlc112358@gmail.com>
2022-09-14 00:04:51 +0800

    added trackformer

--- src/modules/helpers/models/track_transformer.py
+++ src/modules/helpers/models/track_transformer.py
@@ -32,12 +32,12 @@ class TrackFormer(nn.Module):
         b, l, e = tokens.size()
 
         if self.previous_encoder_output is not None and self.previous_encoder_output.size(0) == tokens.size(0):
-            encoder_input = torch.cat((self.previous_encoder_output, tokens), -1)
+            encoder_input = torch.cat((self.previous_encoder_output.cuda(), tokens), -1)
         else:
             zero_padding = torch.zeros(b, l, e) if not self.args.use_cuda else torch.zeros(b, l, e).cuda()
             encoder_input = torch.cat((zero_padding, tokens), -1)
 
-        self.previous_encoder_output = tokens
+        self.previous_encoder_output = tokens.detach().cpu()
 
         encode_out = self.encoder(encoder_input, mask)
         encode_out = self.encoder_embedding(encode_out)
@@ -47,7 +47,7 @@ class TrackFormer(nn.Module):
         b, l, e = tokens.size()
 
         if self.previous_decoder_output is not None and self.previous_decoder_output.size(0) == tokens.size(0):
-            decoder_input = torch.cat((self.previous_decoder_output, tokens), -1)
+            decoder_input = torch.cat((self.previous_decoder_output.cuda(), tokens), -1)
         else:
             zero_padding = torch.zeros(b, l, e) if not self.args.use_cuda else torch.zeros(b, l, e).cuda()
             decoder_input = torch.cat((zero_padding, tokens), -1)
@@ -55,8 +55,8 @@ class TrackFormer(nn.Module):
         decode_out = self.decoder(decoder_input, encode_out, mask, mask, self.args.max_agents_len, False)
         decode_out = self.decoder_embedding(decode_out)
 
-        self.previous_decoder_output = decode_out
+        self.previous_decoder_output = decode_out.detach().cpu()
 
-        return decode_out, self.previous_encoder_output
+        return decode_out, self.previous_encoder_output.cuda()
 
 
