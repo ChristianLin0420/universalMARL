6e49238b10922d149facf33a841c44ac367bbad8
ChristianLin0420 <crlc112358@gmail.com>
2022-09-14 11:40:34 +0800

    added gpt_agent

--- src/controllers/basic_controller.py
+++ src/controllers/basic_controller.py
@@ -97,7 +97,7 @@ class BasicMAC:
     def load_models(self, path):
         self.agent.load_state_dict(th.load("{}/agent.th".format(path), map_location=lambda storage, loc: storage))
 
-        if self.args.agent in ["transfermer"]:
+        if self.args.agent in ["transfermer", "gpt"]:
             self.agent.load_query(path)
             self.agent.fixed_models_weight()
 
