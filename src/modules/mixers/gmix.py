7d0b544b3488183d70954fc88f09be312361d113
ChristianLin0420 <crlc112358@gmail.com>
2022-12-04 21:11:38 +0800

    finished gmix

--- src/modules/mixers/gmix.py
+++ src/modules/mixers/gmix.py
@@ -51,7 +51,8 @@ class GMixer(nn.Module):
 
     def agent_qs_padding(self, agent_qs):
         
-        padding_size = self.args.max_ally_num - agent_qs.size(2)
+        # padding_size = self.args.max_ally_num - agent_qs.size(2)
+        padding_size = self.emb_dim - agent_qs.size(2)
         padding = th.zeros(agent_qs.size(0), agent_qs.size(1), padding_size).to(self.args.device)
         agent_qs = th.cat([agent_qs, padding], axis = -1)
 
