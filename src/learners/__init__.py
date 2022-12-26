52419b4c6bc7f04f5b9a7ee8afc73c88526bdc11
ChristianLin0420 <crlc112358@gmail.com>
2022-12-26 11:29:59 +0800

    finished added qplex

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -2,6 +2,7 @@ from .q_learner import QLearner
 from .coma_learner import COMALearner
 from .qtran_learner import QLearner as QTranLearner
 from .transformer_learner import TransLearner
+from .dmaq_qatten_learner import DMAQ_qattenLearner
 
 REGISTRY = {}
 
@@ -9,3 +10,4 @@ REGISTRY["q_learner"] = QLearner
 REGISTRY["coma_learner"] = COMALearner
 REGISTRY["qtran_learner"] = QTranLearner
 REGISTRY["trans_learner"] = TransLearner
+REGISTRY["dmaq_qatten_learner"] = DMAQ_qattenLearner
